4e37305ab745f6261397cf2dcdcf6d050336f25a
DragonSky2357 <qkrdydals327@gmail.com>
2025-07-27 15:25:02 +0900

    feat: 공통 코드 CommonCode 단일로 수정

--- common/src/main/java/com/dragonsky/nextpage/domain/commoncode/domain/CodeId.java
+++ common/src/main/java/com/dragonsky/nextpage/domain/commoncode/domain/CodeId.java
@@ -2,31 +2,21 @@ package com.dragonsky.nextpage.domain.commoncode.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
-import lombok.Getter;
+import lombok.*;
 
 import java.io.Serializable;
 import java.util.Objects;
 
 @Embeddable
-@Getter
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
 public class CodeId implements Serializable {
+
     @Column(name = "group_code", length = 40, nullable = false)
     private String groupCode;
 
     @Column(name = "common_code", length = 40, nullable = false)
     private String commonCode;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof CodeId)) return false;
-        CodeId that = (CodeId) o;
-        return Objects.equals(groupCode, that.groupCode) &&
-                Objects.equals(commonCode, that.commonCode);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(groupCode, commonCode);
-    }
 }
--- common/src/main/java/com/dragonsky/nextpage/domain/commoncode/domain/CommonCode.java
+++ common/src/main/java/com/dragonsky/nextpage/domain/commoncode/domain/CommonCode.java
@@ -9,13 +9,10 @@ import org.hibernate.annotations.SQLRestriction;
 import org.hibernate.annotations.Where;
 
 @Entity
-@Table(
-        name = "common_code",
-        uniqueConstraints = {
-                @UniqueConstraint(columnNames = {"group_code", "common_code"})
-        }
-)
-@SQLDelete(sql = "UPDATE common_code SET is_active = false, is_deleted = true WHERE id = ?")
+@Table(name = "common_code",
+        uniqueConstraints = @UniqueConstraint(columnNames = {"group_code", "common_code"}))
+@SQLDelete(sql = "UPDATE common_code SET is_active = false, is_deleted = true WHERE group_code = ? AND common_code = ?")
+@Where(clause = "is_active = true AND is_deleted = false")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CommonCode extends BaseEntity {
@@ -31,4 +28,11 @@ public class CommonCode extends BaseEntity {
 
     @Column(name = "sort_order", nullable = false, columnDefinition = "INT DEFAULT 1")
     private Integer sortOrder = 1;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumns({
+            @JoinColumn(name = "parent_group_code", referencedColumnName = "group_code", insertable = false, updatable = false),
+            @JoinColumn(name = "parent_common_code", referencedColumnName = "common_code", insertable = false, updatable = false)
+    })
+    private CommonCode parentCode;
 }
